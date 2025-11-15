b87e68ec5a270dbfa38fc291aeee2549a496a598
Ajay Gour <ajaygour369@gmail.com>
2025-11-15 20:29:31 +0530

    Bootstrap Project Started

---
+++ BasicJavaScript/Day_01/index.js
@@ -0,0 +1,16 @@
+var a = 10;
+var a = 20;
+
+let b = 10;
+console.log(a);
+const x = 0;
+let arr = ["Abc", 123, 45.32, 67];
+console.log(arr);
+console.warn("ye warning hai");
+console.error("ye error hai");
+// alert("This is a alert");
+if (confirm("are you sure")) {
+  console.log("thanks");
+} else {
+  console.log("its oky");
+}
