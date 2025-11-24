83ccb553f0ad10dce5e1ace29c4bd07265fd78b1
Ajay Gour <ajaygour369@gmail.com>
2025-11-24 23:59:44 +0530

    btn

--- BasicJavaScript/Practice/ColorChanger/script.js
+++ BasicJavaScript/Practice/ColorChanger/script.js
@@ -1,16 +1,23 @@
 const usercolor = document.getElementById("color");
 const userheading = document.getElementById("color2");
 const userpara = document.getElementById("color3");
-
-usercolor.addEventListener("change", () => changedcontainerColor(usercolor.value));
-function  changedcontainerColor(color) {
+const btn = document.getElementById("btn1");
+usercolor.addEventListener("change", () =>
+  changedcontainerColor(usercolor.value)
+);
+function changedcontainerColor(color) {
   document.getElementById("container").style.backgroundColor = color;
 }
 userheading.addEventListener("change", () => changeHeading(userheading.value));
-function  changeHeading(color) {
+function changeHeading(color) {
   document.getElementById("heading").style.color = color;
 }
 userpara.addEventListener("change", () => changeparaColor(userpara.value));
-function  changeparaColor(color) {
+function changeparaColor(color) {
   document.getElementById("para").style.color = color;
+}
+btn.addEventListener("change", () => clickbtn(btn.value));
+function clickbtn(color) {
+  document.getElementById("btn").style.backgroundColor = color;
+  document.getElementById("btn").style.color = color;
 }
