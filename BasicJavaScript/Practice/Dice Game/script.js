811bc7a725c3ddd6e430e1279dfe2968fe881cd3
Ajay Gour <ajaygour369@gmail.com>
2025-11-26 20:53:57 +0530

    Dice Game Project

---
+++ BasicJavaScript/Practice/Dice Game/script.js
@@ -0,0 +1,39 @@
+function Start() {
+  console.log("game start");
+
+  document.getElementById("playerOneDice").disabled = false;
+  document.getElementById("playerTwoDice").disabled = false;
+  document.getElementById("RestartBtn").disabled = false;
+  document.getElementById("Startbtn").disabled = true;
+}
+function Restart() {
+  window.location.reload();
+}
+function PlayerOne() {
+    console.log("Player One start");
+  let score = Number(document.getElementById("playerOneScore").innerText);
+  const DF = Math.floor(Math.random() * 6) + 1;
+//   switch(DF){
+//     case:1:document.image url copy and paste break
+//   }
+  if (DF === 6) {
+    document.getElementById("playerOneDice").disabled = true;
+    document.getElementById("playerTwoDice").disabled = false;
+  }
+  score = score + DF;
+  console.log(DF);
+  document.getElementById("playerOneScore").innerText = score;
+}
+
+function PlayerTwo() {
+  console.log("Player Two start");
+  let score = Number(document.getElementById("playerTwoScore").innerText);
+  const DF = Math.floor(Math.random() * 6) + 1;
+  if (DF === 6) {
+    document.getElementById("playerOneDice").disabled = false;
+    document.getElementById("playerTwoDice").disabled = true;
+  }
+  score = score + DF;
+  console.log(DF);
+  document.getElementById("playerTwoScore").innerText = score;
+}
