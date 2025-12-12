51bc6f23bdd5510db8493bc585a4315ba4cd9c79
Ajay Gour <ajaygour369@gmail.com>
2025-12-12 15:58:32 +0530

    Poject Guess The Number is ompleted

---
+++ BasicProject/TipCalculator/script.js
@@ -0,0 +1,5 @@
+function Calculate() {
+  const bill = document.getElementById("bill").value;
+  const amount = document.getElementById("Tip").value;
+  const TotalPerson = document.getElementById("TotalPerson").value;
+}
