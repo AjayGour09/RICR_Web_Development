ccb7a74a3861f13216e9195117e28c527d99600a
Ajay Gour <ajaygour369@gmail.com>
2025-12-25 10:20:20 +0530

    onlineShopping

---
+++ AssignmentsProject/OnlineShppingWebsite/script.js
@@ -0,0 +1,10 @@
+// async function FakeApi() {
+//   try {
+//     const api =await fetch("https://fakestoreapi.com/products");
+//     const res =await api.json();
+//     console.log(res.length);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// FakeApi();
