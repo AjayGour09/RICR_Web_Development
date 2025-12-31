2b632e9ad637156b4793ef47f2d7001381e8961b
Ajay Gour <ajaygour369@gmail.com>
2025-12-31 20:21:48 +0530

    Registration form ,  Form Validation , Regex

--- ReactRicr/registration-form/src/App.jsx
+++ ReactRicr/registration-form/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-
+import { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
 const App = () => {
   const [SubmitRegistrationForm, setSubmitRegistrationForm] = useState({
     firstname: "",
@@ -14,6 +15,7 @@ const App = () => {
   });
 
   const [isLoading, setisLoading] = useState(false);
+  const [validationerror, setvalidationerror] = useState({});
   const handleChange = (e) => {
     const { name, value } = e.target;
     setSubmitRegistrationForm((prev) => ({ ...prev, [name]: value }));
@@ -32,127 +34,194 @@ const App = () => {
     });
   };
 
+  const validatate = () => {
+    let error={};
+    if(!SubmitRegistrationForm.firstname.length <3){
+      error.firstname="First Name must be at least 3 characters long";
+    }else{
+      if(!/^[A-Za-z]+$/.test(SubmitRegistrationForm.firstname)){
+        error.firstname="First Name must contain only letters";
+      }
+      {
+    if(!SubmitRegistrationForm.lastname.length <3){
+      error.lastname="Last Name must be at least 3 characters long";
+    }else{
+      if(!/^[A-Za-z]+$/.test(SubmitRegistrationForm.lastname)){
+        error.lastname="Last Name must contain only letters";
+      }
+    }
+      }
+    }
+    if(!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(SubmitRegistrationForm.email)){
+      error.email="Invalid Email Format";
+    }
+    if(!/^[6-9]\d{9}$/.test(SubmitRegistrationForm.number)){
+      error.number="Invalid Phone Number";
+    }
+    if(SubmitRegistrationForm.State==="--Select--" || SubmitRegistrationForm.State===""){
+      error.State="Please select a State";
+    }
+    if(SubmitRegistrationForm.District==="--Select--" || SubmitRegistrationForm.District===""){
+      error.District="Please select a District";
+    }
+    setvalidationerror(error);
+    return Object.keys(error).length>0?false:true;
+
+  }
   const SubmitData = async (e) => {
     e.preventDefault();
     setisLoading(true);
+    if(!validatate()){
+      setisLoading(false);
+      toast.error("Please fix the validation errors");
+      return;
+    }
     try {
       console.log(SubmitRegistrationForm);
+      toast.success("Form Submitted Successfully");
+      ClearForm();
     } catch (error) {
       console.log(error.message);
+      toast.error(validationerror.message);
     } finally {
       setisLoading(false);
     }
-    ClearForm();
+    
   };
   return (
     <>
+    <Toaster position="top-right" reverseOrder={false} />
       <div>
         <div className="container border w-200 h-160 border-blue-300 flex mx-auto mt-15 rounded-2xl shadow-lg">
-        <form onSubmit={SubmitData} onReset={ClearForm}>
-          <h4 className="ps-60 mt-1.5 text-2xl">Registration Form</h4>
-          <div  className="flex gap-5 justify-center mt-3 ms-5">
-            <div >
-              <label htmlFor="firstname" className="text-2xl p-3 mt-1">First Name</label>
+          <form onSubmit={SubmitData} onReset={ClearForm}>
+            <h4 className="ps-60 mt-1.5 text-2xl">Registration Form</h4>
+            <div className="flex gap-5 justify-center mt-3 ms-5">
+              <div>
+                <label htmlFor="firstname" className="text-2xl p-3 mt-1">
+                  First Name
+                </label>
+               <div>
+                 <input
+                  type="text"
+                  name="firstname"
+                  id="firstname"
+                  value={SubmitRegistrationForm.firstname}
+                  onChange={handleChange}
+                  placeholder="Enter your First Name"
+                  className="border h-fit p-1 mt-2 w-90 rounded-[7px] border-blue-400 shadow-lg"
+                />
+                {validationerror.firstname && (
+                  <span className="text-red-500 text-sm mt-1">
+                    {validationerror.firstname}
+                  </span>
+                )}
+               </div>
+              </div>
+              <div>
+                <label htmlFor="lastname" className="text-2xl p-3 mt-1">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  name="lastname"
+                  id="lastname"
+                  value={SubmitRegistrationForm.lastname}
+                  onChange={handleChange}
+                  placeholder="Enter your Last Name"
+                  className="border p-1 h-fit mt-2 w-90 rounded-[7px] border-blue-400 shadow-lg"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="email" className="text-xl p-3 mt-1 ms-5">
+                Enter Email
+              </label>
               <input
-                type="text"
-                name="firstname"
-                id="firstname"
-                value={SubmitRegistrationForm.firstname}
+                type="email"
+                name="email"
+                id="email"
+                value={SubmitRegistrationForm.email}
                 onChange={handleChange}
-                placeholder="Enter your First Name"
-                className="border p-1 mt-2 w-90 rounded-[7px] border-blue-400 shadow-lg"
+                placeholder="Enter Your Mail"
+                className="border p-1 mt-8 w-160 h-fit rounded-[7px] border-blue-400 shadow-lg"
               />
             </div>
             <div>
-              <label htmlFor="lastname" className="text-2xl p-3 mt-1" >Last Name</label>
+              <label htmlFor="number" className="text-xl p-3 mt-1">
+                Your Number
+              </label>
               <input
-                type="text"
-                name="lastname"
-                id="lastname"
-                value={SubmitRegistrationForm.lastname}
+                type="number"
+                name="number"
+                id="number"
+                className="border p-1 mt-8 w-160 rounded-[7px] border-blue-400 shadow-lg"
+                value={SubmitRegistrationForm.number}
                 onChange={handleChange}
-                placeholder="Enter your Last Name"
-                className="border p-1 mt-2 w-90 rounded-[7px] border-blue-400 shadow-lg"
+                placeholder="Enter Your Number"
               />
             </div>
-          </div>
-          <div>
-            <label htmlFor="email" className="text-xl p-3 mt-1 ms-5">Enter Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={SubmitRegistrationForm.email}
-              onChange={handleChange}
-              placeholder="Enter Your Mail"
-              className="border p-1 mt-8 w-160 rounded-[7px] border-blue-400 shadow-lg"
-            />
-          </div>
-          <div>
-            <label htmlFor="number"className="text-xl p-3 mt-1" >Your Number</label>
-            <input
-              type="number"
-              name="number"
-              id="number"className="border p-1 mt-8 w-160 rounded-[7px] border-blue-400 shadow-lg"
-              value={SubmitRegistrationForm.number}
-              onChange={handleChange}
-              placeholder="Enter Your Number"
-            />
-          </div>
-          <div>
-            <label htmlFor="dob" className="text-xl p-3 mt-1">Date-Of-Birth</label>
-            <input
-              type="date"
-              name="dob"
-              id="dob"
-              className="border p-1 mt-8 w-160 rounded-[7px] border-blue-400 shadow-lg"
-              value={SubmitRegistrationForm.dob}
-              onChange={handleChange}
-              placeholder="Date Of Brith"
-            />
-          </div>
-          <div className="flex gap-2 justify-center mt-3">
-            <div className="me-25">
-              <label htmlFor="State" className="text-xl me-4">State</label>
-              <select
-                name="State"
-                id="State"
-                className="border p-1 mt-8 w-60 rounded-[7px] border-blue-400 shadow-lg"
-                value={SubmitRegistrationForm.State}
-                onChange={handleChange}
-              >
-                <option value="--Select--"></option>
-                <option value="Madhya-Pradesh">Madhya-Pradesh</option>
-                <option value="Bihar">Bihar</option>
-                <option value="Uttar-Pradesh">Uttar-Pradesh</option>
-                <option value="Delhi">Delhi</option>
-                <option value="Kolkata">Kolkata</option>
-                <option value="Jammu-Kashmir">Jammu-Kashmir</option>
-                <option value="Other">Other</option>
-              </select>
-            </div>
             <div>
-              <label htmlFor="District" className="text-xl me-5">District</label>
-              <select
-                name="District"
-                id="District"
-                className="border p-1 mt-8 w-60 rounded-[7px] border-blue-400 shadow-lg"
-                value={SubmitRegistrationForm.District}
+              <label htmlFor="dob" className="text-xl p-3 mt-1">
+                Date-Of-Birth
+              </label>
+              <input
+                type="date"
+                name="dob"
+                id="dob"
+                className="border p-1 mt-8 w-160 rounded-[7px] border-blue-400 shadow-lg"
+                value={SubmitRegistrationForm.dob}
                 onChange={handleChange}
-              >
-                <option value="--Select--"></option>
-                <option value="Bhopal">Bhopal</option>
-                <option value="Reeva">Reeva</option>
-                <option value="Morena">Morena</option>
-                <option value="Sheopur">Sheopur</option>
-                <option value="Shivpuri">Shivpuri</option>
-                <option value="Indore">Indore</option>
-                <option value="Other">Other</option>
-              </select>
+                placeholder="Date Of Brith"
+              />
             </div>
-          </div>
-          <div className="flex gap-5 mt-8">
-              <label htmlFor="gender" className="text-2xl ms-8 ">Gender</label>
+            <div className="flex gap-2 justify-center mt-3">
+              <div className="me-25">
+                <label htmlFor="State" className="text-xl me-4">
+                  State
+                </label>
+                <select
+                  name="State"
+                  id="State"
+                  className="border p-1 mt-8 w-60 rounded-[7px] border-blue-400 shadow-lg"
+                  value={SubmitRegistrationForm.State}
+                  onChange={handleChange}
+                >
+                  <option value="--Select--"></option>
+                  <option value="Madhya-Pradesh">Madhya-Pradesh</option>
+                  <option value="Bihar">Bihar</option>
+                  <option value="Uttar-Pradesh">Uttar-Pradesh</option>
+                  <option value="Delhi">Delhi</option>
+                  <option value="Kolkata">Kolkata</option>
+                  <option value="Jammu-Kashmir">Jammu-Kashmir</option>
+                  <option value="Other">Other</option>
+                </select>
+              </div>
+              <div>
+                <label htmlFor="District" className="text-xl me-5">
+                  District
+                </label>
+                <select
+                  name="District"
+                  id="District"
+                  className="border p-1 mt-8 w-60 rounded-[7px] border-blue-400 shadow-lg"
+                  value={SubmitRegistrationForm.District}
+                  onChange={handleChange}
+                >
+                  <option value="--Select--"></option>
+                  <option value="Bhopal">Bhopal</option>
+                  <option value="Reeva">Reeva</option>
+                  <option value="Morena">Morena</option>
+                  <option value="Sheopur">Sheopur</option>
+                  <option value="Shivpuri">Shivpuri</option>
+                  <option value="Indore">Indore</option>
+                  <option value="Other">Other</option>
+                </select>
+              </div>
+            </div>
+            <div className="flex gap-5 mt-8">
+              <label htmlFor="gender" className="text-2xl ms-8 ">
+                Gender
+              </label>
               <input
                 type="radio"
                 name="gender"
@@ -182,22 +251,37 @@ const App = () => {
               Other
             </div>
 
-          <div className="flex mt-1 gap-8">
-            <div className="text-2xl mt-1 ms-8"><label htmlFor="Address">Address</label></div>
-           <div> <textarea
-              name="address"
-              id="address"
-               className="border p-1 mt-2 w-150 rounded-[7px] border-blue-400 shadow-lg" 
-              value={SubmitRegistrationForm.address}
-              onChange={handleChange}
-            ></textarea></div>
-          </div>
-          <div className="flex gap-5 justify-center mt-5 mb-5">
-            <button type="submit" className="border p-3 rounded-2xl text-blue-300 border-blue-500 hover:border-amber-200 shadow-lg">Submit Data</button>
-            <button type="reset" className="border p-3 rounded-2xl text-blue-300 border-blue-500 hover:border-amber-200 shadow-lg">Reset Data</button>
-          </div>
-        </form>
-      </div>
+            <div className="flex mt-1 gap-8">
+              <div className="text-2xl mt-1 ms-8">
+                <label htmlFor="Address">Address</label>
+              </div>
+              <div>
+                {" "}
+                <textarea
+                  name="address"
+                  id="address"
+                  className="border p-1 mt-2 w-150 rounded-[7px] border-blue-400 shadow-lg"
+                  value={SubmitRegistrationForm.address}
+                  onChange={handleChange}
+                ></textarea>
+              </div>
+            </div>
+            <div className="flex gap-5 justify-center mt-5 mb-5">
+              <button
+                type="submit"
+                className="border p-3 rounded-2xl text-blue-300 border-blue-500 hover:border-amber-200 shadow-lg"
+              >
+                Submit Data
+              </button>
+              <button
+                type="reset"
+                className="border p-3 rounded-2xl text-blue-300 border-blue-500 hover:border-amber-200 shadow-lg"
+              >
+                Reset Data
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
