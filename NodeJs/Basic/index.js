f09d25a27ab62bd9610eb0be859e4e31994f9a77
Ajay Gour <ajaygour369@gmail.com>
2026-01-06 20:34:41 +0530

    Backend Started

---
+++ NodeJs/Basic/index.js
@@ -0,0 +1,25 @@
+// console.log("Hello ,World");
+
+// let a = 10;
+// if(a%2===0){
+//     console.log("even");
+    
+// }else{
+//     console.log("ODD");
+// }
+function Add(a,b){
+    console.log(a+b);
+    
+}
+function Sub(a,b){
+    console.log(a+b);
+    
+}
+function Mul(a,b){
+    console.log(a*b);
+    
+}
+console.log("abcd");
+
+export { Add,Sub};
+export default Mul;
---
+++ NodeJs/Basic/indextwo.js
@@ -0,0 +1,7 @@
+import {Add , Sub , Mul}from "./index.js";
+let  x = 10;
+let y = 20;
+
+Add(2,3);
+Sub(23,8);
+Mul(5,4);
