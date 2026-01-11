3b2ad7490fac7d230d51fa88099d54934455090b
Ajay Gour <ajaygour369@gmail.com>
2026-01-11 09:15:31 +0530

    server

--- NodeJs/FirstBackend/server.js
+++ NodeJs/FirstBackend/server.js
@@ -2,26 +2,27 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import connectDB from "./src/config/db.js";
-import AuthRouter from "./src/routers/myRouter.js"
+import AuthRouter from "./src/routers/myRouter.js";
 const app = express();
 
 app.use(express.json());
-app.use("/auth",AuthRouter);
+
+app.use("/auth", AuthRouter);
 
 app.get("/", (req, res) => {
-  console.log("server is running");
+  console.log("Server is Running");
+  res.json({ message: "Server is Running Sucessfully" });
+});
 
-app.use((err,req,res,next)=>{
-  const Errormessage =  err.message ||"Internal server error";
-  const Statuscode=err.Statuscode ||500;
+app.use((err, req, res, next) => {
+  const ErrorMessage = err.message || "Internal Server Error";
+  const StatusCode = err.statusCode || 500;
 
-  res.status(Statuscode).json({message:Errormessage});
-})
+  res.status(StatusCode).json({ message: ErrorMessage });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("server started at port", port);
+  console.log("Server Started at port ", port);
   connectDB();
-  res.json({ message: "server is running successfully" });
-});
 });
