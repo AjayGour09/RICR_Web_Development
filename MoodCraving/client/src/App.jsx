3d545869a541910f1b533e97a62eae33f5ceec21
Ajay Gour <ajaygour369@gmail.com>
2026-01-11 19:13:20 +0530

    Connecting Frontend and Backend

---
+++ MoodCraving/client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "./Components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
