091354ad14c2ae4383ff99fe3843fae00f775d2a
Ajay Gour <ajaygour369@gmail.com>
2026-01-13 20:15:22 +0530

    login page

--- MoodCraving/server/src/controllers/authConttollers.js
+++ MoodCraving/server/src/controllers/authConttollers.js
@@ -3,25 +3,29 @@ import bcrypt from "bcrypt";
 
 export const UserRegister = async (req, res, next) => {
   try {
-    //Accept data from frontend
+    //accept data exist
     const { fullName, email, mobileNumber, password } = req.body;
     if (!fullName || !email || !mobileNumber || !password) {
-      const error = new Error("All Fields required");
+      const error = new Error("All feilds reuired");
       error.statusCode = 400;
       return next(error);
     }
-
+    console.log({ fullName, email, mobileNumber, password });
+    
+    //Check for duplicate use before registation
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      const error = new Error("Email Already Registred");
+      const error = new Error("Email already register");
       error.statusCode = 409;
       return next(error);
     }
 
+    //encrypt the password
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
 
-    //save data to database
+    //save data to databse
+
     const newUser = await User.create({
       fullName,
       email,
@@ -29,53 +33,53 @@ export const UserRegister = async (req, res, next) => {
       password: hashPassword,
     });
 
-    //send response tp frontend
+    //sand response to frontend
+
     console.log(newUser);
-    res.status(201).json({ message: "Registration Successfull" });
+
+    res.status(201).json({ message: "Registration Seccessfull" });
+    //End
+  } catch (error) {
+    next(error);
+  }
+};
+export const UserLogin = async (req, res, next) => {
+  try {
+    //Fetch data fronted
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      const error = new Error("All feilds reuired");
+      error.statusCode = 400;
+      return next(error);
+    }
+    //check is user regi\der or not
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      const error = new Error("Email already registerd");
+      error.statusCode = 401;
+      return next(error);
+    }
+    //verfy the password
+
+    const isVerified = await bcrypt.compare(password, existingUser.password);
+    if (!isVerified) {
+      const error = new Error("Email didn't match");
+      error.statusCode = 401;
+      return next(error);
+    }
+
+    //send message to frontend
+    res.status(200).json({ message: "Login Successfull", data: existingUser });
 
     //End
   } catch (error) {
     next(error);
   }
 };
-
-export const UserLogin = async (req, res, next) => {
+export const UserLogout = async (req, res, next) => {
   try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      const error = new Error("All Fields required");
-      error.statusCode = 400;
-      return next(error);
-    }
-
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      const error = new Error("Email Not Registred");
-      error.statusCode = 402;
-      return next(error);
-    }
-
-    //password varify
-
-    const isVarified = await bcrypt.compare(password, existingUser.password);
-    if (!isVarified) {
-      const error = new Error("Password didn't Match"); 
-      error.statusCode = 402;
-      return next(error);
-    }
-
-    //Send Message to Frontend
-    res.statusCode(204).json({ message: "Login Successfull", data:existingUser });
-    //End 
-
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const UserLogout = async (req, res,next) => {
-  try {
-    res.statusCode(204).json({ message: "Logout Successfull" });
+    res.status(200).json({ message: "Login Successfull" });
   } catch (error) {
     next(error);
   }
