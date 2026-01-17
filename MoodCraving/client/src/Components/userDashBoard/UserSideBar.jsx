a61b2d2967411578a2df25c082392a9524ccea72
Ajay Gour <ajaygour369@gmail.com>
2026-01-17 18:07:43 +0530

    hameburger menu

--- MoodCraving/client/src/Components/userDashBoard/UserSideBar.jsx
+++ MoodCraving/client/src/Components/userDashBoard/UserSideBar.jsx
@@ -1,68 +1,78 @@
-import React from "react";
-import { TbChartDonut4 } from "react-icons/tb";
+import React, { useState } from "react";
+import { TbChartTreemap } from "react-icons/tb";
+import { TiShoppingCart } from "react-icons/ti";
 import { ImProfile } from "react-icons/im";
-import { FaBoxTissue } from "react-icons/fa6";
-import { MdOutlineAttachMoney } from "react-icons/md";
-import { LuHandHelping } from "react-icons/lu";
-import { FaHamburger } from "react-icons/fa";
+import { RiCustomerService2Fill } from "react-icons/ri";
+import { TbTransactionRupee } from "react-icons/tb";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineKeyboardDoubleArrowLeft } from "react-icons/md";
 
-const UserSideBar = ({ active, setActive }) => {
+export const UserSideBarLogo = ({ active, setActive, SideBar, setSideBar }) => {
   return (
     <>
-      <div className="p-3">
-        <div className="text-xl font-bold flex justify-center items-center gap-2"> <FaHamburger />User DashBoard</div>
-        <hr />
-        <div className="grid gap-3 p-6 font-semibold">
+      <div className="px-3 py-4 " >
+        <button className="text-xl ps-3 font-bold  hover:animate-spin " onClick={() => setSideBar(true)}  >
+          <GiHamburgerMenu className="text-xl" />
+        </button>
+
+        <div className="grid gap-3 overflow-hidden py-6 ">
           <button
-            onClick={() => setActive("overview")}
-            className={`flex gap-3 items-center  hover:bg-gray-100 p-3 rounded-xl ${
+            className={` hover:bg-gray-100/70 p-3 w-10 rounded-xl ${
               active === "overview"
                 ? "bg-(--color-secondary) text-white"
                 : "hover:bg-gray-100/70"
-            }`}
+            } `}
+            onClick={() => setActive("overview")}
           >
-            {" "}
-            <MdOutlineAttachMoney /> Overview
+            <TbChartTreemap />
           </button>
+
           <button
-              className={`flex gap-3 items-center  hover:bg-gray-100 p-3 rounded-xl ${
-              active === "orders"
-                ? "bg-(--color-secondary) text-white"
-                : "hover:bg-gray-100/70"
-            }`}
-            onClick={() => setActive("orders")}
-          >
-            <TbChartDonut4 /> Orders
-          </button>
-          <button
-            onClick={() => setActive("profile")}
-              className={`flex gap-3 items-center  hover:bg-gray-100 p-3 rounded-xl ${
+            className={`flex items-center gap-3 w-10 hover:bg-gray-100/70 p-3 rounded-xl ${
               active === "profile"
                 ? "bg-(--color-secondary) text-white"
                 : "hover:bg-gray-100/70"
-            }`}
+            } `}
+            onClick={() => setActive("profile")}
           >
-            <ImProfile /> Profile
+            <ImProfile />
+            
           </button>
+
           <button
-            onClick={() => setActive("transaction")}
-              className={`flex gap-3 items-center  hover:bg-gray-100 p-3 rounded-xl ${
-              active === "transaction"
+            className={`flex items-center gap-3 hover:bg-gray-100/70 p-3 rounded-xl w-10 ${
+              active === "orders"
                 ? "bg-(--color-secondary) text-white"
                 : "hover:bg-gray-100/70"
-            }`}
+            } `}
+            onClick={() => setActive("orders")}
           >
-            <FaBoxTissue /> Transaction
+            <TiShoppingCart />
+            
           </button>
+
           <button
-            onClick={() => setActive("helpdesk")}
-              className={`flex gap-3 items-center  hover:bg-gray-100 p-3 rounded-xl ${
+            className={`flex items-center gap-3 hover:bg-gray-100/70 p-3 rounded-xl w-10 ${
+              active === "transactions"
+                ? "bg-(--color-secondary) text-white"
+                : "hover:bg-gray-100/70"
+            } `}
+            onClick={() => setActive("transactions")}
+          >
+            <TbTransactionRupee />
+            
+          </button>
+
+          <button
+            className={`flex items-center gap-3 hover:bg-gray-100/70 p-3 rounded-xl w-10 ${
               active === "helpdesk"
                 ? "bg-(--color-secondary) text-white"
                 : "hover:bg-gray-100/70"
-            }`}
+            } `}
+            onClick={() => setActive("helpdesk")}
           >
-            <LuHandHelping /> HelpDesk
+            <RiCustomerService2Fill />
+            
           </button>
         </div>
       </div>
@@ -70,4 +80,78 @@ const UserSideBar = ({ active, setActive }) => {
   );
 };
 
-export default UserSideBar;
+export const UserSideBar = ({ active, setActive, SideBar, setSideBar }) => {
+  return (
+    <>
+      <div className="p-3">
+        <div className="text-xl font-bold flex gap-2 items-center justify-between">
+          
+          User Dashboard
+          <button  onClick={() => setSideBar(false)} className="hover:animate-ping hover:bg-blue-950 hover:text-white rounded-2xl "><MdOutlineKeyboardDoubleArrowLeft className="text-xl"/></button>
+        </div>
+        <hr />
+        <div className="grid gap-3 p-6">
+          <button
+            className={`flex items-center gap-3 hover:bg-gray-100/70 p-3 rounded-xl ${
+              active === "overview"
+                ? "bg-(--color-secondary) text-white"
+                : "hover:bg-gray-100/70"
+            } `}
+            onClick={() => setActive("overview")}
+          >
+            <TbChartTreemap />
+            Overview
+          </button>
+
+          <button
+            className={`flex items-center gap-3 hover:bg-gray-100/70 p-3 rounded-xl ${
+              active === "profile"
+                ? "bg-(--color-secondary) text-white"
+                : "hover:bg-gray-100/70"
+            } `}
+            onClick={() => setActive("profile")}
+          >
+            <ImProfile />
+            Profile
+          </button>
+
+          <button
+            className={`flex items-center gap-3 hover:bg-gray-100/70 p-3 rounded-xl ${
+              active === "orders"
+                ? "bg-(--color-secondary) text-white"
+                : "hover:bg-gray-100/70"
+            } `}
+            onClick={() => setActive("orders")}
+          >
+            <TiShoppingCart />
+            Orders
+          </button>
+
+          <button
+            className={`flex items-center gap-3 hover:bg-gray-100/70 p-3 rounded-xl ${
+              active === "transactions"
+                ? "bg-(--color-secondary) text-white"
+                : "hover:bg-gray-100/70"
+            } `}
+            onClick={() => setActive("transactions")}
+          >
+            <TbTransactionRupee />
+            Transactions
+          </button>
+
+          <button
+            className={`flex items-center gap-3 hover:bg-gray-100/70 p-3 rounded-xl ${
+              active === "helpdesk"
+                ? "bg-(--color-secondary) text-white"
+                : "hover:bg-gray-100/70"
+            } `}
+            onClick={() => setActive("helpdesk")}
+          >
+            <RiCustomerService2Fill />
+            Help Desk
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
--- MoodCraving/client/src/Pages/DashBoards/UserDashBoard.jsx
+++ MoodCraving/client/src/Pages/DashBoards/UserDashBoard.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import UserSideBar from "../../Components/userDashBoard/UserSideBar";
+import  {
+  UserSideBar,
+  UserSideBarLogo,
+} from "../../Components/userDashBoard/UserSideBar";
 import { useState } from "react";
 import UserOverView from "../../Components/userDashBoard/UserOverView";
 import UserProfile from "../../Components/userDashBoard/UserProfile";
@@ -9,20 +12,44 @@ import UserHelpDesk from "../../Components/userDashBoard/UserHelpDesk";
 
 const UserDashBoard = () => {
   const [active, setActive] = useState("overview");
+  const [SideBar, setSideBar] = useState(false);
 
   return (
     <>
-      <div className="w-full h-[90vh] flex">
-        <div className="border border-green-600 w-1/7 bg-(--color-background)">
-          <UserSideBar active={active} setActive={setActive} />
-        </div>
+      {SideBar ? (
+        <div className="w-full h-[90vh] flex">
+          <div className="border border-green-600 w-1/7 bg-(--color-background)">
+            <UserSideBar active={active} setActive={setActive} SideBar={SideBar} setSideBar={setSideBar} />
+          </div>
 
-        {active == "overview" && <UserOverView />}
-        {active == "profile" && <UserProfile />}
-        {active == "orders" && <UserOrders />}
-        {active == "transaction" && <UserTransaction />}
-        {active == "helpdesk" && <UserHelpDesk />}
-      </div>
+          <div>
+            {active == "overview" && <UserOverView />}
+            {active == "profile" && <UserProfile />}
+            {active == "orders" && <UserOrders />}
+            {active == "transaction" && <UserTransaction />}
+            {active == "helpdesk" && <UserHelpDesk />}
+          </div>
+        </div>
+      ) : (
+        <div className="w-80 h-[90vh] flex">
+          <div className="border border-green-600 w-2/10 bg-(--color-background)">
+            <UserSideBarLogo
+              active={active}
+              setActive={setActive}
+              SideBar={SideBar}
+              setSideBar={setSideBar}
+            />
+          </div>
+
+          <div className="w-8/10">
+            {active == "overview" && <UserOverView />}
+            {active == "profile" && <UserProfile />}
+            {active == "orders" && <UserOrders />}
+            {active == "transaction" && <UserTransaction />}
+            {active == "helpdesk" && <UserHelpDesk />}
+          </div>
+        </div>
+      )}
     </>
   );
 };
