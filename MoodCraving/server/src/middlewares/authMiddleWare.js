2f0c487e286c24eb577b4b0d8cf6e2be3f39ea86
Ajay Gour <ajaygour369@gmail.com>
2026-02-01 20:13:07 +0530

    feat: Implement OTP verification and password reset functionality; enhance auth middleware and token generation

--- MoodCraving/server/src/middlewares/authMiddleWare.js
+++ MoodCraving/server/src/middlewares/authMiddleWare.js
@@ -4,21 +4,47 @@ import User from "../models/userModel.js";
 export const Protect = async (req, res, next) => {
   try {
     const biscut = req.cookies.parleG;
-    console.log("Token received in cookies:", biscut);
+    console.log("Token recived in Cookies:", biscut);
 
-    if (!biscut) {
-      const error = new Error("Unauthorized! Please login again");
+    const tea = jwt.verify(biscut, process.env.JWT_SECRET);
+    console.log(tea);
+    if (!tea) {
+      const error = new Error("Unauthorized! Please Login Again");
       error.statusCode = 401;
       return next(error);
     }
 
-    const tea = jwt.verify(biscut, process.env.JWT_SECRET);
-    console.log("Decoded token:", tea);
-
     const verifiedUser = await User.findById(tea.id);
-
     if (!verifiedUser) {
-      const error = new Error("Unauthorized! Please login again");
+      const error = new Error("Unauthorized! Please Login Again");
+      error.statusCode = 401;
+      return next(error);
+    }
+
+    req.user = verifiedUser;
+    
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+export const OtpProtect = async (req, res, next) => {
+  try {
+    const token = req.cookies.otpToken;
+    console.log("Token recived in Cookies:", token);
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decode);
+    if (!decode) {
+      const error = new Error("Unauthorized! Please try again");
+      error.statusCode = 401;
+      return next(error);
+    }
+
+    const verifiedUser = await User.findById(decode.id);
+    if (!verifiedUser) {
+      const error = new Error("Unauthorized! Please try again");
       error.statusCode = 401;
       return next(error);
     }
@@ -29,3 +55,4 @@ export const Protect = async (req, res, next) => {
     next(error);
   }
 };
+
